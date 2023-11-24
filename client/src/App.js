d10d9c48503abf4facec645366759543ceb135bb
prajktamhase <mhaseprajkta@gmail.com>
2023-11-24 11:18:28 +0530

    app completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import copy from "./copy.png"
+import axios from "axios";
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [slug, setSlug] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+  const [links,setLinks]=useState([]);
+
+  const generateLink = async () => {
+    const response = await axios.post("./link", {
+      url,
+      slug
+    })
+    setShortUrl(response?.data?.data?.shortUrl)
+  }
+
+
+  const copyUrl = () => {
+    navigator.clipboard.writeText(shortUrl)
+    alert('copy to clipboard')
+  }
+
+  const loadLinks= async ()=>{
+    const response= await axios.get('/api/links');
+    setLinks(response?.data?.data)
+  }
+
+  useEffect(()=>{
+    loadLinks();
+  },[])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center">
+      <h1>Quick Links🖇️</h1>
+
+      <div className='container'>
+        <div className='link-card'>
+          <h2>Link Generation</h2>
+
+          <input type='text'
+            placeholder='URL'
+            value={url}
+            className='box'
+            onChange={(e) => {
+              setUrl(e.target.value)
+            }}
+          /><br />
+
+          <input type='text'
+            placeholder='Slug (optional'
+            value={slug}
+            className='box'
+            onChange={(e) => {
+              setSlug(e.target.value)
+            }}
+          /><br />
+
+          <div className='conteiner-flex'>
+            <b><input type='text'
+              placeholder='Short Url'
+              value={shortUrl}
+              className='inputbox'
+              disabled /></b>
+            <br />
+
+            <img src={copy} alt="copy"
+              className='copy-icon'
+              onClick={copyUrl} />
+
+          </div>
+
+          <button type='button'
+            className='btn'
+            onClick={generateLink}>
+            Do Magic🪄
+          </button>
+        </div>
+
+        <div className='links-text-container'>
+
+      
+
+          {
+            links?.map((linkObj,index)=>{
+               const {url,slug,clicks}=linkObj;
+              return(
+                <>
+                <div key={index} className='link-content'>
+                  <p className='link-text'>URL :        {url}</p>
+                  <p>Short Url : {process.env.REACT_APP_BASE_URL}/{slug}</p>
+                  <p>Clicks : {clicks}</p>
+                </div>
+                </>
+
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,44 +4,76 @@ import dotenv from "dotenv"
 dotenv.config();
 import Link from "./models/Link.js";
 
-const app=express();
+const app = express();
 app.use(express.json());
 
-const connectDB = async ()=>{
-const conn =await mongoose.connect(process.env.MONGODB_URL);
-if(conn){
-    console.log(`MongoDB connected...💖!`);
-}
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    if (conn) {
+        console.log(`MongoDB connected...💖!`);
+    }
 };
 connectDB();
 
-app.post("/link",async(req,res)=>{
-    const {url , slug} = req.body;
-const randomslug = Math.random().toString(36).substring(2,7);
+app.post("/link", async (req, res) => {
+    const { url, slug } = req.body;
+    const randomslug = Math.random().toString(36).substring(2, 7);
 
-const link=new Link({
-    url,
-    slug :slug || randomslug,
-})
-
-try{
-    const saveLink=await link.save();
-    return res.json({
-        success:true,
-        data:saveLink,
-        message:"Link saved successfully"
-    });
-}
-catch(err){
-    return res.json({
-        success:false,
-        message:err.message,
+    const link = new Link({
+        url:url,
+        slug: slug || randomslug,
     })
-}
+
+    try {
+        const saveLink = await link.save();
+        return res.json({
+            success: true,
+            data:
+            {
+                shortUrl: `${process.env.BASE_URL}/${saveLink.slug}`
+            },
+            message: "Link saved successfully"
+        });
+    }
+    catch (err) {
+        return res.json({
+            success: false,
+            message: err.message,
+        })
+    }
 })
 
-const PORT=process.env.PORT || 5000;
-app.listen(PORT,()=>{
+app.get("/:slug", async (req, res) => {
+    const { slug } = req.params;
+
+    const link = await Link.findOne({ slug: slug });
+
+    await Link.updateOne({slug : slug},{$set:{
+            clicks:link.clicks+1
+        }})
+
+    if (!link) {
+        return res.json({
+            success: false,
+            message: "Link not found"
+        })
+    }
+    res.redirect(link.url);
+})
+
+app.get('/api/links',async(req,res)=>{
+    const links = await Link.find();
+
+    res.json({
+        success:true,
+        data:links,
+        message:'Links fetch successfully'
+    })
+})
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log(`server is start ${PORT}`);
 });
 
